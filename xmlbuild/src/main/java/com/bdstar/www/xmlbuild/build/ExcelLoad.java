d1a4b5fc714658e7300f1b297a2cc20d67a2d043
wangpeng <2865673691@qq.com>
2018-10-23 18:44:32 +0800

    181023_2

--- xmlbuild/src/main/java/com/bdstar/www/xmlbuild/build/ExcelLoad.java
+++ xmlbuild/src/main/java/com/bdstar/www/xmlbuild/build/ExcelLoad.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.sql.SQLException;
 
+import jxl.Cell;
+import jxl.Range;
 import jxl.Sheet;
 import jxl.Workbook;
 import jxl.read.biff.BiffException;
@@ -24,12 +26,32 @@ public class ExcelLoad {
             InputStream is = new FileInputStream(file.getAbsolutePath());
             Workbook wb = Workbook.getWorkbook(is);
             int sheetSize = wb.getNumberOfSheets();
-            System.out.println("sheetSize-->"+sheetSize);
-            for (int index = 0; index < sheetSize; index++) {
+            System.out.println("sheetSize-->" + sheetSize);
+            for (int index = 0; index < 1; index++) {
                 Sheet sheet = wb.getSheet(index);
-                System.out.println("getRows-->"+sheet.getName());
-                System.out.println("getRows-->"+sheet.getRows());
-                System.out.println("getColumns-->"+sheet.getColumns());
+                System.out.println("getRows-->" + sheet.getName());
+                System.out.println("getRows-->" + sheet.getRows());
+                System.out.println("getColumns-->" + sheet.getColumns());
+
+
+
+                Cell cellYj = sheet.findCell("快速入门");
+                System.out.println("cellYj-->" + cellYj.getContents());
+                System.out.println("getRow-->" + cellYj.getRow());
+                System.out.println("getColumn-->" + cellYj.getColumn());
+                Range[] merged = sheet.getMergedCells();
+                for (Range range : merged) {
+                    System.out.println("-->"+range);
+
+                    System.out.println("getTopLeft-->"+range.getTopLeft().getContents());
+                    System.out.println("getFirstSheetIndex-->"+range.getBottomRight().getRow());
+                }
+
+//                Cell[] column = sheet.getColumn(0);
+//                for (Cell cell : column) {
+//                    System.out.println("-->"+cell.getContents());
+//                }
+
                 // sheet.getRows()返回该页的总行数
                 // sheet.getColumns()返回该页的总列数
 //                for (int i = 0; i < sheet.getRows(); i++) {
