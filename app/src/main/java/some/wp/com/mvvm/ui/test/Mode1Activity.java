572c5c309847eb6e2f6968cd8ec56babfe315c31
wangpeng <2865673691@qq.com>
2019-01-25 18:42:33 +0800

    190125_1

---
+++ app/src/main/java/some/wp/com/mvvm/ui/test/Mode1Activity.java
@@ -0,0 +1,16 @@
+package some.wp.com.mvvm.ui.test;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import some.wp.com.mvvm.R;
+
+public class Mode1Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mode1);
+    }
+
+}
